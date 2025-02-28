9802870b8a4456c9d64e25c04a15dece75fdaea0
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-02-28 14:09:48 +0900

    Title: 세 수의 합과 평균 구하기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250228/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
@@ -0,0 +1,12 @@
+user = input()
+arr = user.split()
+
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+if 1 <= a <= 100 and 1 <= b <= 100 and 1 <= c <= 100 :
+    print(a + b + c)
+    print(int((a + b + c) / 3))
+else :
+    print("error")
