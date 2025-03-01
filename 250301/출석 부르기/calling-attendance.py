e94da4e2cd8174b2abd6607322f9eac2dba3de55
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-03-01 16:58:38 +0900

    Title: 출석 부르기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250301/출석 부르기/calling-attendance.py
@@ -0,0 +1,13 @@
+num = int(input())
+
+if 1 <= num <= 100 :
+    if num == 1 :
+        print("John")
+    elif num == 2 :
+        print("Tom")
+    elif num == 3 :
+        print("Paul")
+    else :
+        print("Vacancy")
+else :
+    print("error")
