cc5f81e7abf68c0e8c200c0e0873097814a81760
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-03-01 15:23:44 +0900

    Title: 큰 수에서 빼기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250301/큰 수에서 빼기/subtract-from-large-number.py
@@ -0,0 +1,13 @@
+user = input()
+arr = user.split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+if 1 <= a <= 100 and 1 <= b <= 100 :
+    if a > b :
+        print(a - b)
+    else :
+        print(b - a)
+else :
+    print("error")
