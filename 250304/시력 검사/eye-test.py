86047fbba90544e899c0cbf1d3791351ba2afe1a
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-03-04 22:10:26 +0900

    Title: 시력 검사, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250304/시력 검사/eye-test.py
+++ 250304/시력 검사/eye-test.py
@@ -4,7 +4,7 @@ b = float(input())
 if 0 <= a <= 5 and 0 <= b <= 5 :
     if a >= 1.0 and b >= 1.0 :
         print("High")
-    elif 1.0 > a >= 0.5 and 1.0 > b >= 0.5 :
+    elif a >= 0.5 and b >= 0.5 :
         print("Middle")
     else :
         print("Low")
