6c4ab2e111e6af642ae4f6cfd1a35248360b4b98
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-03-05 13:20:21 +0900

    Title: 두 사람, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250305/두 사람/two-person.py
@@ -0,0 +1,19 @@
+f = input()
+s = input()
+
+arr_f = f.split()
+arr_s = s.split()
+
+f_a = int(arr_f[0])
+f_s = arr_f[1]
+
+s_a = int(arr_s[0])
+s_s = arr_s[1]
+
+if 1 <= f_a <= 100 and 1 <= s_a <= 100 :
+    if (f_a >= 19 or s_a >= 19) and (f_s == "M" or s_s == "M") :
+        print(1)
+    else :
+        print(0)
+else :
+    print("error")
