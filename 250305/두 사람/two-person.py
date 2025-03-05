96988823a0dcb48a4f63720ceddb1a43b5b02ddd
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-03-05 13:25:29 +0900

    Title: 두 사람, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250305/두 사람/two-person.py
+++ 250305/두 사람/two-person.py
@@ -1,4 +1,4 @@
-f = input()
+''' f = input()
 s = input()
 
 arr_f = f.split()
@@ -16,4 +16,21 @@ if 1 <= f_a <= 100 and 1 <= s_a <= 100 :
     else :
         print(0)
 else :
-    print("error")
+    print("error") '''
+
+f = input()
+s = input()
+
+arr_f = f.split()
+arr_s = s.split()
+
+f_a = int(arr_f[0])
+f_s = arr_f[1]
+
+s_a = int(arr_s[0])
+s_s = arr_s[1]
+
+if (f_a >= 19 and f_s == "M") or (s_a >= 19 and s_s == "M"):
+    print(1)
+else:
+    print(0)
