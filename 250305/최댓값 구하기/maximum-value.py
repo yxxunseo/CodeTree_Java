c3832fb5d911a5784a5c025e2c1b2fbe1b98b43c
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-03-05 14:50:29 +0900

    Title: 최댓값 구하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250305/최댓값 구하기/maximum-value.py
+++ 250305/최댓값 구하기/maximum-value.py
@@ -6,19 +6,15 @@ b = int(arr[1])
 c = int(arr[2])
 
 if -100 <= a <= 100 and -100 <= b <= 100 and -100 <= c <= 100 :
-    if a > 0 and b > 0 and c > 0:
-        if a > b and b > c :
+    if a >= b :
+        if a >= c :
             print(a)
-        elif b > a and b > c :
-            print(b)
         else :
             print(c)
-    if a < 0 and b < 0 and c < 0 :
-        if a < b and a < c :
-            print(a)
-        elif b < a and b < c :
+    else :
+        if b >= c :
             print(b)
-        else :
+        else : 
             print(c)
 else :
     print("error")
