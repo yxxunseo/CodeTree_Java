38985ed0759ffaebf51e8344890123e611ff22dc
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-03-05 14:18:17 +0900

    Title: 일 수 구하기, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250305/일 수 구하기/number-of-days-in-month.py
+++ 250305/일 수 구하기/number-of-days-in-month.py
@@ -1,29 +1,17 @@
 n = int(input())
 
 if 1 <= n <= 12 :
-    if n == 1 :
-        print("31")
-    if n == 2 :
-        print("28")
-    if n == 3 :
-        print("31")
-    if n == 4 :
-        print("30")
-    if n == 5 :
-        print("31")
-    if n == 6 :
-        print("30")
-    if n == 7 :
-        print("31")
-    if n == 8 :
-        print("30")
-    if n == 9 :
-        print("31")
-    if n == 10 :
-        print("30")
-    if n == 11 :
-        print("31")
-    if n == 12 :
-        print("30")
+    if n == 2:
+	    print("28")
+    elif n <= 7:
+        if n % 2 == 1:
+            print("31")
+        else:
+            print("30")
+    else:
+        if n % 2 == 0:
+            print("31")
+        else:
+            print("30")
 else :
     print("error")
