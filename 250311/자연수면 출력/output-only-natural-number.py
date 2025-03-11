0e385ce346ef014110c69ca49f7caf0d9ff5b9bd
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-03-11 23:38:30 +0900

    Title: 자연수면 출력, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250311/자연수면 출력/output-only-natural-number.py
@@ -0,0 +1,11 @@
+a, b = input().split()
+a, b = int(a), int(b)
+
+if -9 <= a <= 9 and 1 <= b <= 50 :
+    if a > 0 :
+        for i in range(b) :
+            print(a, end = "")
+    if a < 0 :
+        print("0")
+else :
+    print("error")
