c5354a2433fe49aec1345272e061a1ea1e89ec2f
yxxunseo <161916112+yxxunseo@users.noreply.github.com>
2025-03-11 23:35:30 +0900

    Title: 높은 수에서 낮은 수까지, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250311/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,12 @@
+a, b = input().split()
+a, b = int(a), int(b)
+
+if 1 <= a <= 50 and 1 <= b <= 50 :
+    if a > b :
+        for i in range(a, b-1, -1) :
+            print(i, end = " ")
+    if b > a :
+        for i in range(b, a-1, -1) :
+            print(i, end = " ")
+else :
+    print("error")
