434d759c253b0f413c19e415df2e7d403b1c55bd
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-14 21:09:11 +0900

    Title: 데이터 교환, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250714/데이터 교환/exchange-data.swift
@@ -0,0 +1,15 @@
+var a = 5
+var b = 6
+var c = 7
+var temp1 = 0
+var temp2 = 0
+
+temp1 = b //temp1=6
+temp2 = c //temp2=7
+b = a // b=5
+c = temp1 //c=6
+a = temp2 //a=7
+
+print(a)
+print(b)
+print(c)
