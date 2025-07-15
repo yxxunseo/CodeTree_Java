ecd9a0c00044a31e39e7c6d3d9cde0e968f66e8d
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-15 14:38:19 +0900

    Title: 입력받아 계산 5, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250715/입력받아 계산 5/input-calculate-5.swift
@@ -0,0 +1,10 @@
+var input = readLine()!.split(separator: " ").map { Int($0)! }
+var a = input[0]
+var b = input[1]
+
+if (1 <= a && a <= 100) && (1 <= b && b <= 100) {
+    print(a + b)
+}
+else {
+    print("Error")
+}
