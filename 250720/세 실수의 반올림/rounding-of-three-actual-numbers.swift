ab485591a3a8637ea20643db11b713a26718927e
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-20 00:36:37 +0900

    Title: 세 실수의 반올림, Time: 13ms, Memory: 1MB, Status: Passed - Codetree

--- 250720/세 실수의 반올림/rounding-of-three-actual-numbers.swift
+++ 250720/세 실수의 반올림/rounding-of-three-actual-numbers.swift
@@ -4,7 +4,7 @@ let a = Double(readLine()!)!
 let b = Double(readLine()!)!
 let c = Double(readLine()!)!
 
-if (1 <= a && a <= 1000) && (1 <= b && b <= 1000) && (1 <= c && c <= 1000) {
+if (0 <= a && a <= 1000) && (0 <= b && b <= 1000) && (0 <= c && c <= 1000) {
     print(String(format: "%.3f", a))
     print(String(format: "%.3f", b))
     print(String(format: "%.3f", c))
