503928de3e63a17a2354e20c18fcc0d8fdb4b64c
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-20 00:54:08 +0900

    Title: 실수와 문자 받아 출력하기, Time: 14ms, Memory: 1MB, Status: Passed - Codetree

--- 250720/실수와 문자 받아 출력하기/print-number--and-letter.swift
+++ 250720/실수와 문자 받아 출력하기/print-number--and-letter.swift
@@ -4,7 +4,7 @@ let c = readLine()!
 let a = Double(readLine()!)!
 let b = Double(readLine()!)!
 
-if (1 <= a && a <= 1000) && ((1 <= b && b <= 1000)) {
+if (0 <= a && a <= 1000) && ((0 <= b && b <= 1000)) {
     print(c)
     print(String(format: "%.2f", a))
     print(String(format: "%.2f", b))
