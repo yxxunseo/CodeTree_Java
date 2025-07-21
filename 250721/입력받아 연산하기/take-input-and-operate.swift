c6a6c97abe08fe4f68b4b75e438fac50bcb0e758
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-21 09:43:23 +0900

    Title: 입력받아 연산하기, Time: 11ms, Memory: 1MB, Status: Passed - Codetree

--- 250721/입력받아 연산하기/take-input-and-operate.swift
+++ 250721/입력받아 연산하기/take-input-and-operate.swift
@@ -1,7 +1,7 @@
 let a = Int(readLine()!)!
 let b = Int(readLine()!)!
 
-if (1 <= a && a <= 100) && (1 <= b && b <= 100) {
+if (1 <= a && a <= 500) && (1 <= b && b <= 500) {
     print(a + 87)
     print(b % 10)
 }
