f5b34fb826840e84a2b63d2df1a8e098e492eaa4
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-21 09:08:38 +0900

    Title: 몫과 나머지, Time: 11ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250721/몫과 나머지/quotient-remainder.swift
@@ -0,0 +1,7 @@
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let a = input[0]
+let b = input[1]
+
+if (1 <= a && a <= 100) && (1 <= b && b <= 100) {
+    print("\(a / b)...\(a % b)")
+}
