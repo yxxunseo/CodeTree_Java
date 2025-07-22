259af3275172ebf4c8b10ab9e9b27ef7e3c25268
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-22 15:13:58 +0900

    Title: 알파벳에 따른 평가, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250722/알파벳에 따른 평가/evaluation-by-alphabet.swift
@@ -0,0 +1,20 @@
+let input = readLine()!
+
+if input == "S" {
+    print("Superior")
+}
+else if input == "A" {
+    print("Excellent")
+}
+else if input == "B" {
+    print("Good")
+}
+else if input == "C" {
+    print("Usually")
+}
+else if input == "D" {
+    print("Effort")
+}
+else {
+    print("Failure")
+}
