7b65a2d1454ffb12a334da705a4338810693e530
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-22 15:09:25 +0900

    Title: 출석 부르기, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250722/출석 부르기/calling-attendance.swift
@@ -0,0 +1,16 @@
+let num = Int(readLine()!)!
+
+if 1 <= num && num <= 100 {
+    if num == 1 {
+        print("John")
+    }
+    else if num == 2 {
+        print("Tom")
+    }
+    else if num == 3 {
+        print("Paul")
+    }
+    else {
+        print("Vacancy")
+    }
+}
